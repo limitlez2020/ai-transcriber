8b8f574786d661773d92ba2949b764fdfc62aaa7
fortune.akinremi <fortuneakinremi@gmail.com>
2024-11-28 14:16:45 -0500

    Added some changes to the display the transcript

--- app/components/SpeechToText.js
+++ app/components/SpeechToText.js
@@ -41,10 +41,12 @@ export default function SpeechToText() {
 
     /* Update the transcript: */
     recognition.onresult = async function (event) {
-      const results = Array.from(event.results)
-        .map((result) => result[0].transcript)
-        .join(""); /* Join the partial results */
-      setTranscript(results);
+      const transcript = event.results[0][0].transcript;
+      setTranscript(transcript);
+      // const results = Array.from(event.results)
+      //   .map((result) => result[0].transcript)
+      //   .join(""); /* Join the partial results */
+      // setTranscript(results);
     }
 
     /* Handle errors: */
